f82f138c5d319c5cff54aac8f11861f36d99aa84
Ahadul Haque Shovo <suvo10.info@gmail.com>
2025-08-14 12:59:11 +0600

    Create prime_number.js

---
+++ prime_number.js
@@ -0,0 +1,17 @@
+function isprime(num){
+    if(num<= 1)
+        return false ;
+    for(let i = 2 ; i<=Math.sqrt(num);i++){
+        if(num%i==0)
+            return false ;
+    } 
+    return true ;
+}
+// example usage 
+const number =17 ;
+if(isprime(number)){
+    console.log(number+"is a prime number");
+}
+else {
+    console.log(number+"is not a prime number");
+}
