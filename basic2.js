82f4bc1d28af404bc2cd137cf33a45b82a69fb5d
Shuvo <suvo10.info@gmail.com>
2025-11-15 02:13:17 +0600

    kvbbig

--- basic2.js
+++ basic2.js
@@ -2,11 +2,23 @@
 //let s = "123";
 //let n = Number(s);  // Converts string to number
 //console.log(n);
-let bool = true ; 
-let s=String(bool);
-console.log(s)
+//let bool = true ; 
+//let s=String(bool);
+//console.log(s)
 // er por dekhi 
 // automatic type conversion korbo 
+let n = 5 ; 
+let m = "5";// string nilam 
+let res= n+m ;  // string ar number er jog 
+console.log(res);
+
+// bool ar number er jog korbo nau 
+let bool = true ; 
+let dol = 10 ; 
+let res1 = bool + dol ;
+console.log(res1);
+
+
 
 
 
