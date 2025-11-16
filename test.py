d9bc20c2370a73152644fa5049833497ee31403b
Shuvo <suvo10.info@gmail.com>
2025-11-16 16:08:02 +0600

    fjkgfjk

--- test.py
+++ test.py
@@ -2,3 +2,8 @@ from pymongo import MongoClient
 client = MongoClient("mongodb://localhost:27017/")
 db = client["test_db"]
 print("Connected to MongoDB Server!")
+collection = db["students"]
+collection.insert_one({"name": "Shuvo", "age": 22})
+
+for doc in collection.find():
+    print(doc)
