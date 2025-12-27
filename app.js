20781683ccbef60598148f2e939546290622606b
Shuvo <suvo10.info@gmail.com>
2025-12-27 16:55:54 +0600

    fhdfh

--- app.js
+++ app.js
@@ -6,4 +6,4 @@ app.listen(3000,()=> {
        console.log('app listening on port 3000')
 })
 // routes 
-app.get('/books',(req,res)=> { res.json()})
+app.get('/books',(req,res)=> { res.json({mssg:"welcome to the api"})})
