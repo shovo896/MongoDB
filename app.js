3522078709963c20caedda5f9718849573e1d367
Shuvo <suvo10.info@gmail.com>
2025-12-27 20:06:13 +0600

    gijvhv

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ const express=require('express')
 const {connectToDb,getDb}=require('./db')
 // init app 
 const app = express()
+let db
+
 connectToDb((err) => {
        if(!err){
               app.listen(3000,()=> {
        console.log('app listening on port 3000')})
+       db=getDb()
 
        }
 
