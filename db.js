5306de178ba4361b438f47206a0954e48309ee62
Shuvo <suvo10.info@gmail.com>
2025-12-27 19:21:55 +0600

    giuifg

--- db.js
+++ db.js
@@ -1,12 +1,13 @@
 
 const {MongoClient}=require('mongodb')
-
+let dbConnection
 
 module.exports={
        connectToDb:() => {MongoClient.connect('mongodb://localhost:27017/bookstore')
               .then((client) => {
-                     client.db()
+                     dbConnection=client.db()
               })
+              .catch((err) => {})
        },
        getDb : () => {}
 }
