cc49a7416cee68f81606fe9de78c079f92435dbb
Shuvo <suvo10.info@gmail.com>
2025-12-27 16:54:28 +0600

    gyfgy

---
+++ app.js
@@ -0,0 +1,9 @@
+const express=require('express')
+
+// init app 
+const app = express()
+app.listen(3000,()=> {
+       console.log('app listening on port 3000')
+})
+// routes 
+app.get('/books',(req,res)=> { res.json()})
