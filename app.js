9f6c1f9bfc18c225c3831c65e1140d3ebc1ca0a4
Shuvo <suvo10.info@gmail.com>
2025-12-27 19:58:51 +0600

    ghjjf

--- app.js
+++ app.js
@@ -2,12 +2,16 @@ const express=require('express')
 const {connectToDb,getDb}=require('./db')
 // init app 
 const app = express()
-connectToDb(() => {
-       
+connectToDb((err) => {
+       if(!err){
+              app.listen(3000,()=> {
+       console.log('app listening on port 3000')})
+
+       }
+
 })
-app.listen(3000,()=> {
-       console.log('app listening on port 3000')
-})
+
+
 // routes 
 app.get('/books',(req,res)=> { res.json({mssg:"welcome to the api"})})
 
