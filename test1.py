eb4e5f5b513643199303afe503bc3a2c4ac8985a
Shuvo <suvo10.info@gmail.com>
2026-01-28 00:44:54 +0600

    bij

--- test1.py
+++ test1.py
@@ -1,17 +1,33 @@
 import psycopg2
+from psycopg2 import sql
 
-conn = psycopg2.connect(
-    host="localhost",
-    database="testdb",
-    user="postgres",
-    password="1234"  # 
-)
+DB_NAME = "testdb"
+CONN_KW = {
+    "host": "localhost",
+    "user": "postgres",
+    "password": "1234",  # 
+}
 
-cur = conn.cursor()
-cur.execute("SELECT version();")
 
-print("Connected!")
-print(cur.fetchone())
+def connect_db(dbname: str) -> psycopg2.extensions.connection:
+    return psycopg2.connect(database=dbname, **CONN_KW)
 
-cur.close()
-conn.close()
+
+try:
+    conn = connect_db(DB_NAME)
+except psycopg2.OperationalError as exc:
+    if f'database "{DB_NAME}" does not exist' in str(exc):
+        admin_conn = connect_db("postgres")
+        admin_conn.autocommit = True
+        with admin_conn.cursor() as cur:
+            cur.execute(sql.SQL("CREATE DATABASE {}").format(sql.Identifier(DB_NAME)))
+        admin_conn.close()
+        conn = connect_db(DB_NAME)
+    else:
+        raise
+
+with conn:
+    with conn.cursor() as cur:
+        cur.execute("SELECT version();")
+        print("Connected!")
+        print(cur.fetchone())
